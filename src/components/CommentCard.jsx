0a2f341003b7a975ea98d3ff572405adae69561a
Nikita Lendiak <lendiak93@gmail.com>
2023-03-14 16:46:26 +0000

    fix DB overloading

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,24 +1,15 @@
 import { useEffect, useState } from "react";
-import { fetchUser } from "../utils/api";
+import { fetchUser, postNewComment } from "../utils/api";
 import { formatCreatedAt } from "../utils/formatCreatedAt";
 import Votes from "./Votes";
 
 const CommentCard = ({ comment: { body, author, votes, created_at } }) => {
-  const [user, setUser] = useState({});
   const formattedCreatedAt = formatCreatedAt(created_at);
-
-  useEffect(() => {
-    fetchUser(author).then((user) => {
-      setUser(user);
-    });
-  }, [author]);
 
   return (
     <li>
       <article>
-        {user.avatar_url && (
-          <img src={user.avatar_url} alt={`${author}'s avatar`} />
-        )}
+        <p>{author[0].toUpperCase()}</p>
         <p>
           {author} <time dateTime={created_at}>{formattedCreatedAt}</time>
         </p>
