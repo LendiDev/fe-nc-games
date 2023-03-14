7b4285b5108bc5cca32255105cb4d7e8c07004e0
Nikita Lendiak <lendiak93@gmail.com>
2023-03-14 10:17:32 +0000

    add error component

---
+++ src/components/ErrorSection.jsx
@@ -0,0 +1,22 @@
+import { useNavigate } from "react-router-dom";
+import SectionHeader from "./SectionHeader";
+
+const ErrorSection = ({ showHeader = false, title, message }) => {
+  const navigate = useNavigate();
+
+  const handleBackToSafety = () => {
+    navigate("/");
+  };
+
+  return (
+    <section>
+      {showHeader && (
+        <SectionHeader title={title ? title : "Something went wrong..."} />
+      )}
+      <p>{message}</p>
+      <button onClick={handleBackToSafety}>Go back to safety</button>
+    </section>
+  );
+};
+
+export default ErrorSection;
