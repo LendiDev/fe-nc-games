2ecbafbb2c63f03ac6fe9434e548e4c1fb7ef1ab
Nikita Lendiak <lendiak93@gmail.com>
2023-03-14 08:36:02 +0000

    add mobile nav toggling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,5 @@
+import { IoMenu, IoClose } from "react-icons/io5";
+import { Link } from "react-router-dom";
 import NavigationBar from "./NavigationBar";
 
 const Header = ({ isMobileNavOpened, setIsMobileNavOpened }) => {
@@ -6,23 +8,28 @@ const Header = ({ isMobileNavOpened, setIsMobileNavOpened }) => {
   };
 
   return (
-    <header className="main-header">
-      <h1 className="main-header__logo">NC BoardGames</h1>
-      <NavigationBar
-        isMobileNavOpened={isMobileNavOpened}
-        setIsMobileNavOpened={setIsMobileNavOpened}
-      />
-      <button className="mobile-nav__btn" onClick={onHandlerMobileNavOpened}>
-        {!isMobileNavOpened ? (
-          <>
-            <span />
-            <span />
-            <span />
-          </>
-        ) : (
-          <p className="mobile-nav__btn__close">X</p>
-        )}
-      </button>
+    <header className="main-header-container">
+      <div className="main-header">
+        <h1 className="main-header__logo">
+          <Link to="/">NC BoardGames</Link>
+        </h1>
+        <NavigationBar
+          isMobileNavOpened={isMobileNavOpened}
+          setIsMobileNavOpened={setIsMobileNavOpened}
+        />
+        <div className="clear-flex-1" />
+        <button
+          className="mobile-nav__btn"
+          onClick={onHandlerMobileNavOpened}
+          aria-label="open navigation menu"
+        >
+          {!isMobileNavOpened ? (
+            <IoMenu className="icon--medium" />
+          ) : (
+            <IoClose className="icon--medium" />
+          )}
+        </button>
+      </div>
     </header>
   );
 };
