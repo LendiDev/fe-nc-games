4d0af9efdd119d40a8beeae30bd7ad60b13ad65c
Nikita Lendiak <lendiak93@gmail.com>
2023-03-14 08:37:05 +0000

    add base to review card

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
+import { BiComment, BiUpvote, BiDownvote } from "react-icons/bi";
 import { dashCaseToHumanReadableString } from "../utils/dashCaseToHumanReadableString";
+import { truncateWithEllipses } from "../utils/truncateWithEllipses";
+import { formatCreatedAt } from "../utils/formatCreatedAt";
 
 const ReviewCard = ({
   review: {
     review_id,
     title,
     category,
-    designer,
     owner,
     review_body,
     review_img_url,
@@ -15,26 +17,60 @@ const ReviewCard = ({
     comment_count,
   },
 }) => {
+  const formattedDate = formatCreatedAt(created_at);
+  const categoryName = dashCaseToHumanReadableString(category);
+  const shotDescription = truncateWithEllipses(review_body, 150);
+
   return (
     <li>
-      <article>
-        <p>{review_img_url}</p>
+      <article className="review-card">
+        <img className="review-card__image" src={review_img_url} alt={title} />
+        <div className="review-card__content">
+          <Link to={`/review/${review_id}`}>
+            <h3>{title}</h3>
+          </Link>
+          <Link
+            to={`/reviews/${category}`}
+            className="review-card__category"
+            aria-label={`Go to reviews in category ${categoryName}`}
+          >
+            {categoryName}
+          </Link>
+          <p className="review-card__author__info">
+            {owner} on <time datetime={created_at}>{formattedDate}</time>
+          </p>
+          <p className="review-card__body">
+            {shotDescription}
+            <Link
+              className="review-card__body__link"
+              to={`/review/${review_id}`}
+            >
+              Read more
+            </Link>
+          </p>
 
-        <Link to={`/review/{review_id}`}>
-          <h3>{title}</h3>
-        </Link>
-        <p>{dashCaseToHumanReadableString(category)}</p>
-
-        <p>
-          Reviewed by: {owner} at {created_at}
-        </p>
-
-        <p>{review_body}</p>
-        <Link to={`/review/{review_id}`}>Read more...</Link>
-        <p>
-          Board game designer by: {designer} | Comments: {comment_count} |
-          Votes: {votes}
-        </p>
+          <div className="review-card__footer">
+            <div>
+              <BiComment className="review-card__comments__icon" />
+              {comment_count} comments
+            </div>
+            <div className="votes">
+              <button className="votes__downvote">
+                <BiDownvote
+                  className="votes__icon votes__downvote"
+                  aria-label="Down vote this review"
+                />
+              </button>
+              <p aria-label="rating">{votes}</p>
+              <button className="votes__upvote">
+                <BiUpvote
+                  className="votes__icon votes__upvote"
+                  aria-label="Up vote this review"
+                />
+              </button>
+            </div>
+          </div>
+        </div>
       </article>
     </li>
   );
