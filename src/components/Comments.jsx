a8cf85deafad358b7f650f7e6bcce7c3bca333a6
Nikita Lendiak <lendiak93@gmail.com>
2023-03-14 11:36:59 +0000

    add comments component

---
+++ src/components/Comments.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import { fetchCommentsForReview } from "../utils/api";
+import CommentCard from "./CommentCard";
+import SectionHeader from "./SectionHeader";
+
+const Comments = ({ review_id }) => {
+  const [comments, setComments] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetchCommentsForReview(review_id)
+      .then(({ comments }) => {
+        setComments(comments);
+      })
+      .catch(() => {
+        setError("Something went wrong... Couldn't not fetch comments.");
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [review_id]);
+
+  return (
+    <section>
+      <SectionHeader title="Comments" />
+      {error && <p>{error}</p>}
+      {isLoading && <p>Loading...</p>}
+      {comments && comments.length > 0 && (
+        <ul>
+          {comments.map((comment) => (
+            <CommentCard key={comment.comment_id} comment={comment} />
+          ))}
+        </ul>
+      )}
+      {comments && comments.length === 0 && <p>No comments yet.</p>}
+    </section>
+  );
+};
+
+export default Comments;
