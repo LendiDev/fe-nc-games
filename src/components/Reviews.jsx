168508b566e367bcb1324d76b4ed5b04d04727fc
Nikita Lendiak <lendiak93@gmail.com>
2023-03-14 08:35:17 +0000

    move state lower

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,15 +1,32 @@
+import { useEffect, useState } from "react";
+import { fetchReviews } from "../utils/api";
+
 import ReviewCard from "./ReviewCard";
 import SectionHeader from "./SectionHeader";
 
-const Reviews = ({ reviews }) => {
+const Reviews = () => {
+  const [reviews, setReviews] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetchReviews().then((reviewsData) => {
+      setIsLoading(false);
+      setReviews(reviewsData.reviews);
+    });
+  }, []);
+
   return (
     <section>
       <SectionHeader title="Reviews" />
-      <ul>
-        {reviews.map((review) => (
-          <ReviewCard key={review.review_id} review={review} />
-        ))}
-      </ul>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {reviews.map((review) => (
+            <ReviewCard key={review.review_id} review={review} />
+          ))}
+        </ul>
+      )}
     </section>
   );
 };
--- src/pages/ReviewsPage.jsx
+++ src/pages/ReviewsPage.jsx
@@ -1,19 +1,9 @@
-import { useEffect, useState } from "react";
 import Reviews from "../components/Reviews";
-import { fetchReviews } from "../utils/api";
 
 const ReviewsPage = () => {
-  const [reviews, setReviews] = useState([]);
-
-  useEffect(() => {
-    fetchReviews().then((reviewsData) => {
-      setReviews(reviewsData.reviews);
-    });
-  }, []);
-
   return (
     <main>
-      <Reviews reviews={reviews} />
+      <Reviews />
     </main>
   );
 };
