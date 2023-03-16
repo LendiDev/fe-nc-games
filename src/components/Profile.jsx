349fcc50896bcae55f43e369094dcc66cd839576
Nikita Lendiak <lendiak93@gmail.com>
2023-03-16 10:57:18 +0000

    add login styling

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,11 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { UserContext } from "../contexts/User.context";
+import { useIsMobile } from "../hooks/useIsMobile";
 
 const Profile = () => {
   const { user } = useContext(UserContext);
+  const isMobile = useIsMobile();
 
   return (
     <div className="main-header-profile">
@@ -15,9 +17,16 @@ const Profile = () => {
             src={user.avatar_url}
             alt={`Profile of ${user.username}`}
           />
+          {!isMobile && (
+            <Link className="logout__link" to="/user/logout">
+              Logout
+            </Link>
+          )}
         </>
       ) : (
-        <Link className="main-header-profile__link" to="/user/login">Login</Link>
+        <Link className="main-header-profile__link" to="/user/login">
+          Login
+        </Link>
       )}
     </div>
   );
