ae7d5366cf93827c7416a219fe08f9311df8672b
Nikita Lendiak <lendiak93@gmail.com>
2023-03-17 02:21:15 +0000

    refactor votes

--- src/components/Votes.jsx
+++ src/components/Votes.jsx
@@ -2,64 +2,46 @@ import { useState } from "react";
 import { BiUpvote, BiDownvote } from "react-icons/bi";
 import { updateComment, updateReview } from "../utils/api";
 
-const Votes = ({ review_id, comment_id, votes: originalVotes, dividerPosition = '' }) => {
-  const [votesCount, setVotesCount] = useState(originalVotes);
-
-  const votesDifference = votesCount - originalVotes;
-
-  const handleUpVote = () => {
-    updateVotesBy(1);
-  };
-
-  const handleDownVote = () => {
-    updateVotesBy(-1);
-  };
+const Votes = ({ review_id, comment_id, votes, dividerPosition = "" }) => {
+  const [votedCount, setVotedCount] = useState(0);
 
   const updateVotesBy = (increaseBy) => {
-    const newVotesDifference = votesDifference + increaseBy;
-
-    if (newVotesDifference > 1 || newVotesDifference < -1) {
-      return;
-    }
-
     const updateRequest = review_id ? updateReview : updateComment;
     const id = review_id || comment_id;
 
-    setVotesCount((currentVotesCount) => currentVotesCount + increaseBy);
+    setVotedCount((voted) => voted + increaseBy);
     updateRequest(id, {
       inc_votes: increaseBy,
     }).catch(() => {
-      setVotesCount((currentVotesCount) => currentVotesCount - increaseBy);
+      setVotedCount((voted) => voted - increaseBy);
     });
   };
-
-  const votesColorStyle =
-    votesDifference === 0
-      ? ""
-      : votesDifference > 0
-      ? "votes--voted_up"
-      : "votes--voted_down";
 
   return (
     <div className={`votes votes__divider--${dividerPosition}`}>
       <button
         className="votes__button votes__downvote"
-        onClick={handleDownVote}
+        onClick={() => updateVotesBy(-1)}
+        disabled={votedCount <= -1}
       >
         <BiDownvote
-          className={`votes__icon votes__downvote ${
-            votesDifference < 0 ? "votes--voted_down" : ""
+          className={`votes__icon votes__downvote${
+            votedCount === -1 ? "--disabled" : ""
           }`}
           aria-label="Down vote this review"
         />
       </button>
-      <p className={votesColorStyle} aria-label={`${votesCount} review votes`}>
-        {votesCount}
+      <p aria-label={`${votes + votedCount} review votes`}>
+        {votes + votedCount}
       </p>
-      <button className="votes__button votes__upvote" onClick={handleUpVote}>
+      <button
+        className="votes__button votes__upvote"
+        onClick={() => updateVotesBy(1)}
+        disabled={votedCount >= 1}
+      >
         <BiUpvote
-          className={`votes__icon votes__upvote ${
-            votesDifference > 0 ? "votes--voted_up" : ""
+          className={`votes__icon votes__upvote${
+            votedCount === 1 ? "--disabled" : ""
           }`}
           aria-label="Up vote this review"
         />
